587d92314a21edc605d1ef57a4044d77915660d8
lvlit <player14017@gmail.com>
2024-05-24 13:13:00 +0500

    Сделали бота который скачивает фото пользователя в папку каталога

--- instloader.py
+++ instloader.py
@@ -1,6 +1,19 @@
-def main():
-    pass
+import instaloader
 
+# Инициализация Instaloader
+L = instaloader.Instaloader()
 
-if __name__ == "__main__":
-    main()
+# Учетные данные (если нужен доступ к приватным аккаунтам)
+# username = 'your_username'
+# password = 'your_password'
+#
+# # Войти в систему (необязательно, если аккаунт публичный)
+# L.login(username, password)
+
+# Имя пользователя, чьи посты нужно скачать
+target_username = 'asruamina3'
+
+# Скачивание постов
+profile = instaloader.Profile.from_username(L.context, target_username)
+for post in profile.get_posts():
+    L.download_post(post, target=target_username)
